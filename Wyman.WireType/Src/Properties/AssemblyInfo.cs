f49c23ff55a74bc4f111bd01627ffdb1f798fe3f
J Wyman <jeremy.wyman@outlook.com>
2017-07-13 23:55:53 -0400

    Initial commit of the WireType IDL Compiler.
    Add Visual Studio solution, primary project, and test project. Establish all of the simple things like which compiler, assortment of GUIDs, and names.

---
+++ Wyman.WireType/Src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WireType IDL Compiler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WireType IDL Compiler")]
+[assembly: AssemblyCopyright("Copyright © J Wyman 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("19770407-8696-481b-8c2d-7cf58099673a")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: InternalsVisibleTo("wtc_test")]
---
+++ Wyman.WireType/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WireType IDL Compiler Test Suite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WireType IDL Compiler Test Suite")]
+[assembly: AssemblyCopyright("Copyright © J Wyman 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("19781214-10f8-4f86-b6e9-c3360b052dbc")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
